967835b944cfd92ab4b8921b584df535e7f86c61
Gaurav <gauravnagal@hotmail.com>
2019-11-20 19:01:09 -0500

    Update HW1_Python_Refresher.py

--- HW1_Python_Refresher.py
+++ HW1_Python_Refresher.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 '''
 Author: Gaurav Nagal
@@ -12,36 +10,33 @@ Assignment 1: Python Refresher
 '''
 
 
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[20]:
+def plot_graphs():
+
+    # Taking one period (2 * pi) on X-axis
+    X = np.arange(0, 2 * np.pi, 0.001)
 
 
-# Taking one period (2 * pi) on X-axis
-X = np.arange(0, 2 * np.pi, 0.001)
+    # Sine and Cosine plot for one period
+    sine = np.sin(X)
+    cosine = np.cos(X)
+    tangent = np.tan(X)
+    
+    # tangent = np.tan(2*(X - np.pi/2))
+    threshold = 5
+    tangent[tangent > threshold] = np.inf
+    tangent[tangent < -threshold] = np.inf
+    
+    # Generating plot
+    plt.plot(X, sine, X, cosine, X, tangent)
+    plt.subplot().legend(['Sine', 'Cosine', 'Tangent'])
+    plt.subplot().axvline(x=0, color='black', lw=2.5)
+    plt.subplot().axhline(y=0, color='black', lw=2.5)
+    
+    plt.show()
 
-# Sine and Cosine plot for one period
-sine = np.sin(X)
-cosine = np.cos(X)
-tangent = np.tan(X)
-
-# tangent = np.tan(2*(X - np.pi/2))
-threshold = 5
-tangent[tangent>threshold] = np.inf
-tangent[tangent<-threshold] = np.inf
-# In[ ]:
-# Generating plot
-plt.plot(X, sine, X, cosine, X, tangent)
-plt.subplot().legend(['Sine','Cosine', 'Tangent'])  
-plt.subplot().axvline(x = 0, color = 'black', lw = 2.5)
-plt.subplot().axhline(y = 0, color = 'black', lw = 2.5)
-
-plt.show()
-
-
-
+if __name__ == '__main__':
+    plot_graphs()
